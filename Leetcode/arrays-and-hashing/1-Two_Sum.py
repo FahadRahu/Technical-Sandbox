e64b26338248d7539a8c923352d7f5c34f8ad16f
FahadRahu <fahadrahu@gmail.com>
2026-03-22 23:16:49 -0400

    Re-solved Leetcode 1 - Two Sum

--- Leetcode/arrays-and-hashing/1-Two_Sum.py
+++ Leetcode/arrays-and-hashing/1-Two_Sum.py
@@ -9,3 +9,22 @@ class Solution:
                 return [prevMap[diff], i]
             prevMap[n] = i
         return []
+
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val: index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return []
+    
+    def twoSum_two_for_loops(self, nums: List[int], target: int) -> List[int]:
+        # Two for loops, O(n^2) time complexity
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
