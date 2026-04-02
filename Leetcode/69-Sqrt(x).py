6ace74017d600469392586ee2902775c22ab203d
FahadRahu <fahadrahu@gmail.com>
2026-04-02 00:09:29 -0400

    Re-Solved Leetcode 69 - Sqrt(x)  - Solved with Binary Search instead of Brute Force

--- Leetcode/69-Sqrt(x).py
+++ Leetcode/69-Sqrt(x).py
@@ -1,6 +1,10 @@
 class Solution:
+    # Brute force approach
     def mySqrt(self, x: int) -> int:
-        res = None
+        res = 0
+
+        if x == 0 or x == 1:
+            return x
 
         for i in range(x):
             temp = i*i
@@ -9,7 +13,24 @@ class Solution:
             if temp > x:
                 break
         return res # type: ignore
+    
+    # Binary search approach
+    def mySqrt2(self, x: int) -> int:
+        res = 0
 
+        if x == 0 or x == 1:
+            return x
+        
+        l, r = 0, x
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                l = mid + 1 # Left pointer moves, right stays the same
+            else: # mid * mid > x --> mid needs to be smaller
+                r = mid - 1
+        # We want to return the last mid that was <= x, which is r, because l will have moved one step past it
+        return r
 """
 Thoughts:
 1. x is positive
