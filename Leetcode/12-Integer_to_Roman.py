c91cacea3436f4460bb47006f61578f68a33555e
FahadRahu <fahadrahu@gmail.com>
2026-04-05 21:53:35 -0400

    Resolved Leetcode 12 - Integer to Roman

--- Leetcode/12-Integer_to_Roman.py
+++ Leetcode/12-Integer_to_Roman.py
@@ -23,3 +23,21 @@ class Solution:
                 res += sym * count
                 num = num % val
         return res
+    
+class Solution2:
+    def intToRoman(self, num: int) -> str:
+        # This is a pretty straightforward problem, we just need to 
+        # create a list of the symbols and their values, and then 
+        # iterate through that list in reverse order, appending the 
+        # symbols to our result string as we go.
+        symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], 
+                   ["XL", 40], ["L", 50], ["XC", 90], ["C", 100], 
+                   ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]]
+        
+        res = "" # Initialize our result string
+        for sym, val in reversed(symList): # Takes Roman symbols and values in reverse order
+            if num // val: # If our number is greater than or equal to the value of the symbol
+                count = num // val
+                res += sym * count # Append the symbol to our result string count times
+                num = num % val # Get the remainder of our number after subtracting the value of the symbol count times
+        return res
