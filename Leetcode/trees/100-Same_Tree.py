aedd2c1807c14aa0089ce0a7813761b729153725
FahadRahu <fahadrahu@gmail.com>
2026-04-19 23:16:12 -0400

    Re-Solved Leetcode 100 - Same Tree

--- Leetcode/trees/100-Same_Tree.py
+++ Leetcode/trees/100-Same_Tree.py
@@ -18,3 +18,16 @@ class Solution:
 
         # Rerun again, twice, this time p and q are the children
         return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+class Solution2: # 4/19/2026
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Using Recursive Depth First Search (DFS)
+        if not p and not q: # Both are null
+            return True
+        if not p or not q: # One is null (other isn't since the above would've passed)
+            return False
+        if p.val != q.val: # If p does not equal q
+            return False
+
+        # Rerun again, twice, this time p and q are the children
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
