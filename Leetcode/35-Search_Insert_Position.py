e6c132b3dc3dbcfbc252a3711a5d6f616f6e7738
Fahad Rahu <fahadrahu@gmail.com>
2026-04-24 22:02:38 -0400

    Re-Solved Leetcode 35 - Search Insert Position

--- Leetcode/35-Search_Insert_Position.py
+++ Leetcode/35-Search_Insert_Position.py
@@ -1,6 +1,7 @@
 from typing import List
 
 class Solution:
+# Re-Solved on 4/24/2026
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
         while left <= right:
