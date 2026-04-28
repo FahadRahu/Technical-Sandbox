3808a3e86b2d7a51d5a9f66c78392cc6a03f064b
Fahad Rahu <fahadrahu@gmail.com>
2026-04-27 20:25:48 -0400

    Re-Solved Leetcode 67 - Add Binary

--- Leetcode/67-Add_Binary.py
+++ Leetcode/67-Add_Binary.py
@@ -1,13 +1,14 @@
 class Solution:
+# Re-Solved 4/27/2026
     def addBinary(self, a: str, b: str) -> str:
         res = ""
         carry = 0
 
-        a, b = a[::-1], b[::-1]
+        a, b = a[::-1], b[::-1] # Start(Inc.) | Stop(Exc.) | Skip(-1 = Start at End)
 
-        for i in range(max(len(a), len(b))):
-            digitA = ord(a[i]) - ord('0') if i < len(a) else 0
-            digitB = ord(b[i]) - ord('0') if i < len(b) else 0
+        for i in range(max(len(a), len(b))): # We'll iterate thru whichever larger string
+            digitA = ord(a[i]) - ord('0') if i < len(a) else 0 #ord gives you the ASCII value of whatever's inputted
+            digitB = ord(b[i]) - ord('0') if i < len(b) else 0 # if cur iteration < len of our string
 
             total = digitA + digitB + carry
             char = str(total % 2)
